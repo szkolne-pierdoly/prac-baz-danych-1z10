67d38fa02a73804e98b002893bda9d452431a110
Nikodem Okroj <n.okroj@icloud.com>
2025-02-25 15:57:41 +0100

    feat: Configure PostgreSQL connection using environment variables

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+string port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+string database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+string username = Environment.GetEnvironmentVariable("POSTGRES_USER");
+string password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+string connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
