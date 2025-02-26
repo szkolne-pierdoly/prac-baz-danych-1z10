aa8e9fb6689dddd8249a1e5719e253c1ef64fdd4
Nikodem Okroj <n.okroj@icloud.com>
2025-02-26 09:37:16 +0100

    chore(frontend): Add Prettier and ESLint configuration for code formatting and linting

--- Frontend/app/providers.tsx
+++ Frontend/app/providers.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 
-import {HeroUIProvider} from "@heroui/react";
+import { HeroUIProvider } from "@heroui/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <HeroUIProvider>
-      {children}
-    </HeroUIProvider>
-  );
+  return <HeroUIProvider>{children}</HeroUIProvider>;
 }
