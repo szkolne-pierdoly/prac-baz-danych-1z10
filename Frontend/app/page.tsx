652f9ef7aaf8cf049a6a029951806f0b517cfe51
Nikodem Okroj <n.okroj@icloud.com>
2025-03-05 13:40:41 +0100

    feat(frontend): Update home page with operator panel layout

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -2,8 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <h1 className="text-6xl text-center">Hello World</h1>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <h1 className="text-center text-6xl">Panel operatora</h1>
+      <div className="flex flex-col items-center justify-center"></div>
     </div>
   );
 }
