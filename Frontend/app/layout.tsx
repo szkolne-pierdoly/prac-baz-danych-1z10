b8e611647221df7fd8711daf7347d72e4e09d21b
Nikodem Okroj <n.okroj@icloud.com>
2025-03-08 20:22:45 +0100

    feat(frontend): Update color scheme and enable dark mode by default

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="dark">
         <Providers>{children}</Providers>
       </body>
     </html>
