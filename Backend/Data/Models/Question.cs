97f42b3cfc1aee8f983949ed29e973ac682b5f4e
Nikodem Okroj <n.okroj@icloud.com>
2025-03-08 20:00:28 +0100

    refactor(backend): Correct Hint2 property casing in Question model

--- Backend/Data/Models/Question.cs
+++ Backend/Data/Models/Question.cs
@@ -5,6 +5,6 @@ public class Question {
     public int Id {get; set;}
     public required string Content {get; set;}
     public required string Hint {get; set;}
-    public string? hint2 {get; set;}
+    public string? Hint2 {get; set;}
     public required string CorrectAnswer {get; set;}
 }
