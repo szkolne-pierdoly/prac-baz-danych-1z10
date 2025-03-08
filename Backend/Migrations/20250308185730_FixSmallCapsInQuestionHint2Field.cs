6ee699bc9d5ef17e5a7c1e4a2fc0e8da72f8424b
Nikodem Okroj <n.okroj@icloud.com>
2025-03-08 20:00:53 +0100

    refactor(backend): Update migration to rename hint2 column to Hint2 in Questions table

---
+++ Backend/Migrations/20250308185730_FixSmallCapsInQuestionHint2Field.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSmallCapsInQuestionHint2Field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "hint2",
+                table: "Questions",
+                newName: "Hint2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Hint2",
+                table: "Questions",
+                newName: "hint2");
+        }
+    }
+}
