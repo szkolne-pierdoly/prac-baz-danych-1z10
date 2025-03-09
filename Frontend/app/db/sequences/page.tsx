56989833f18cc910f2afeb875b476ebb49254d1c
Nikodem Okroj <n.okroj@icloud.com>
2025-03-09 13:24:58 +0100

    feat(frontend): Add sequences page with navigation from home

---
+++ Frontend/app/db/sequences/page.tsx
@@ -0,0 +1,7 @@
+export default function SequencesPage() {
+  return (
+    <div className="flex flex-col items-center justify-start h-screen py-4 gap-4 px-2">
+      <h1>Sequences</h1>
+    </div>
+  );
+}
--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -27,7 +27,11 @@ export default function Home() {
                 <div className="text-2xl font-bold">Pytania: 17</div>
               </CardBody>
             </Card>
-            <Card className="w-full max-w-[25%] min-w-[100px]" isPressable>
+            <Card
+              className="w-full max-w-[25%] min-w-[100px]"
+              isPressable
+              onPress={() => router.push("/db/sequences")}
+            >
               <CardBody>
                 <div className="text-2xl font-bold">Sekwencje: 4</div>
               </CardBody>
