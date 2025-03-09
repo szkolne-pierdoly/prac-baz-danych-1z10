a594fc85bfac53b0c963f42b96e40ab3ef3c7fdb
Nikodem Okroj <n.okroj@icloud.com>
2025-03-09 08:13:07 +0100

    feat(frontend): Implement questions fetching and display on questions page

--- Frontend/app/db/questions/page.tsx
+++ Frontend/app/db/questions/page.tsx
@@ -1,11 +1,44 @@
 "use client";
 
+import { Question } from "../../models/Question";
 import { Button, Card, CardBody } from "@heroui/react";
+import { useEffect, useState } from "react";
 
 export default function QuestionsPage() {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
+  useEffect(() => {
+      fetch(`${apiUrl}/api/questions`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}, url: ${apiUrl}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data && data.questions) {
+          setQuestions(
+            data.questions.map((question: Question) => ({
+              id: question.id,
+              content: question.content,
+              hint: question.hint,
+              hint2: question.hint2,
+              correctAnswer: question.correctAnswer,
+            })),
+          );
+        } else {
+          console.error("Invalid data structure:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  });
+
   return (
-    <div className="flex flex-col items-center justify-start h-screen py-4">
-      <Card className="w-full flex flex-row items-start justify-start max-w-4xl">
+    <div className="flex flex-col items-center justify-start h-screen py-4 gap-4">
+      <Card className="w-full flex flex-row items-start justify-start max-w-5xl">
         <CardBody className="flex flex-row items-center justify-between gap-2">
           <div className="text-2xl font-bold">Pytania</div>
           <div className="flex flex-row items-center justify-center gap-2">
@@ -18,6 +51,14 @@ export default function QuestionsPage() {
           </div>
         </CardBody>
       </Card>
+      <div className="w-full flex flex-col items-start justify-start max-w-4xl">
+        {questions.map((question) => (
+          <div key={question.id} className="flex flex-row gap-2">
+            <div className="text-2xl font-bold">{question.id}</div>
+            <div className="text-lg">{question.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
