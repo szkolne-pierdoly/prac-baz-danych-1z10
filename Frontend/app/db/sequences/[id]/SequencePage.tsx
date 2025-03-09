d92c710acfd38343ee75e83781d10cae7d8068fb
Nikodem Okroj <n.okroj@icloud.com>
2025-03-09 16:09:07 +0100

    feat(frontend): Enhance SequencePage with detailed view and question table

--- Frontend/app/db/sequences/[id]/SequencePage.tsx
+++ Frontend/app/db/sequences/[id]/SequencePage.tsx
@@ -1,7 +1,20 @@
 "use client";
 
 import { Sequence } from "@/app/models/Sequence";
+import {
+  Card,
+  CardBody,
+  Divider,
+  Table,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  TableBody,
+  TableCell,
+  Button,
+} from "@heroui/react";
 import React from "react";
+import { PencilIcon } from "lucide-react";
 
 export default function SequenceClientPage({
   sequence,
@@ -9,8 +22,40 @@ export default function SequenceClientPage({
   sequence: Sequence;
 }) {
   return (
-    <div>
-      <div>SequencePage {sequence?.name}</div>
+    <div className="flex flex-col gap-4 w-full max-w-4xl items-center justify-center">
+      <Card className="w-full">
+        <CardBody className="flex flex-row justify-between items-center">
+          <div className="text-2xl font-bold">Sekwencja: {sequence.name}</div>
+          <Button variant="faded" color="primary">
+            <PencilIcon size={16} />
+            Edytuj
+          </Button>
+        </CardBody>
+        <Divider />
+        <CardBody className="flex flex-col gap-2">
+          <div className="text-lg font-bold">Pytania:</div>
+          <Table removeWrapper>
+            <TableHeader>
+              <TableColumn>ID</TableColumn>
+              <TableColumn>Treść</TableColumn>
+              <TableColumn>Podpowiedź</TableColumn>
+              <TableColumn>Podpowiedź 2</TableColumn>
+              <TableColumn>Odpowiedź</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {sequence.questions.map((question) => (
+                <TableRow key={question.id}>
+                  <TableCell>{question.id}</TableCell>
+                  <TableCell>{question.content}</TableCell>
+                  <TableCell>{question.hint}</TableCell>
+                  <TableCell>{question.hint2??"-"}</TableCell>
+                  <TableCell>{question.correctAnswer}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
     </div>
   );
 }
