2b0f584bace56a55d06bf04abb7d00ef5ff64591
Nikodem Okroj <n.okroj@icloud.com>
2025-03-23 11:40:03 +0100

    feat(backend): Enhance Game model by adding Name and CreatedAt properties, and update migrations to reflect these changes for improved game data management.

--- Backend/Data/Models/Game.cs
+++ Backend/Data/Models/Game.cs
@@ -4,9 +4,11 @@ namespace Backend.Data.Models;
 public class Game
 {
     public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime StartTime { get; set; } = DateTime.UtcNow;
     public DateTime? EndTime { get; set; }
-    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
     public int SequenceId { get; set; }
     public virtual required Sequence Sequence { get; set; }
     public virtual required ICollection<GamePlayer> Players { get; set; }
---
+++ Backend/Migrations/20250323103928_UpdatedGameModel2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedGameModel2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Games",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Games",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Games");
+        }
+    }
+}
