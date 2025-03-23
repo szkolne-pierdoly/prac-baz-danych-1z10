a11d5c5d43d2eea4d99b73d837097b995ccddf4b
Nikodem Okroj <n.okroj@icloud.com>
2025-03-23 19:56:39 +0100

    fix(backend): Refactor Game model to remove default initialization for UpdatedAt and StartTime properties, allowing for explicit setting during game creation.

--- Backend/Data/Models/Game.cs
+++ Backend/Data/Models/Game.cs
@@ -6,8 +6,8 @@ public class Game
     public int Id { get; set; }
     public string? Name { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime StartTime { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? StartTime { get; set; }
     public DateTime? EndTime { get; set; }
     public int SequenceId { get; set; }
     public virtual required Sequence Sequence { get; set; }
