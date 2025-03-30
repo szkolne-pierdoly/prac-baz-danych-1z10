cc03b12213c2033c86d4ef3185390c1680fc6585
Nikodem Okroj <n.okroj@icloud.com>
2025-03-30 19:29:11 +0200

    refactor(frontend): Enhance Game model to include start and end times, game token, and actions

--- Frontend/app/models/Game.ts
+++ Frontend/app/models/Game.ts
@@ -1,14 +1,17 @@
 import { Sequence } from "./Sequence";
-import { Answer } from "./Answer";
 import { GamePlayer } from "./GamePlayer";
+import { GameAction } from "./GameAction";
 
 export interface Game {
   id: number;
   name?: string;
-  sequenceId: number;
-  players: GamePlayer[];
-  answers: Answer[];
-  sequence: Sequence;
   createdAt: Date;
   updatedAt?: Date;
+  startTime?: Date;
+  endTime?: Date;
+  gameToken?: string;
+  sequenceId: number;
+  sequence: Sequence;
+  players: GamePlayer[];
+  actions: GameAction[];
 }
