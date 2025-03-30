b339d534ce2ddcb88fb028ba0822666d552fcc99
Nikodem Okroj <n.okroj@icloud.com>
2025-03-30 20:07:23 +0200

    feat(frontend): Enhance LoadingDialog component to support customizable background opacity

--- Frontend/app/components/loadingDialog.tsx
+++ Frontend/app/components/loadingDialog.tsx
@@ -2,9 +2,19 @@
 
 import { Spinner } from "@heroui/react";
 
-export default function LoadingDialog({ isLoading }: { isLoading: boolean }) {
+export default function LoadingDialog({
+  isLoading,
+  blackBackground = false,
+}: {
+  isLoading: boolean;
+  blackBackground?: boolean;
+}) {
   return isLoading ? (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div
+      className={`fixed inset-0 flex items-center justify-center ${
+        blackBackground ? "bg-black bg-opacity-100" : "bg-black bg-opacity-50"
+      } z-50`}
+    >
       <Spinner size="lg" />
     </div>
   ) : null;
