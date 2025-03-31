88c8e775f89df5162ed40fa15628c8ca8f1b6094
Nikodem Okroj <n.okroj@icloud.com>
2025-03-31 09:08:41 +0200

    feat(backend): Add migration to establish Player and Question relationships in GamePlayer and GameAction models

---
+++ Backend/Migrations/20250331070806_AddedPlayerModelToGamePlayer].cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPlayerModelToGamePlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerId1",
+                table: "GamePlayer",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionId",
+                table: "GameAction",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlayer_PlayerId1",
+                table: "GamePlayer",
+                column: "PlayerId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameAction_QuestionId",
+                table: "GameAction",
+                column: "QuestionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameAction_Questions_QuestionId",
+                table: "GameAction",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GamePlayer_Players_PlayerId1",
+                table: "GamePlayer",
+                column: "PlayerId1",
+                principalTable: "Players",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameAction_Questions_QuestionId",
+                table: "GameAction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GamePlayer_Players_PlayerId1",
+                table: "GamePlayer");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GamePlayer_PlayerId1",
+                table: "GamePlayer");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GameAction_QuestionId",
+                table: "GameAction");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId1",
+                table: "GamePlayer");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionId",
+                table: "GameAction");
+        }
+    }
+}
