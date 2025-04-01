9758b3b0919fcfebddf97c7c60db6bdb4703e37a
Nikodem Okroj <n.okroj@icloud.com>
2025-04-01 19:17:39 +0200

    refactor(backend): Remove UpdatedAt property from Game model to streamline data structure

--- Backend/Data/Models/Game.cs
+++ Backend/Data/Models/Game.cs
@@ -6,7 +6,6 @@ public class Game
     public int Id { get; set; }
     public string? Name { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; set; }
     public DateTime? StartTime { get; set; }
     public DateTime? EndTime { get; set; }
     public string? GameToken { get; set; }
