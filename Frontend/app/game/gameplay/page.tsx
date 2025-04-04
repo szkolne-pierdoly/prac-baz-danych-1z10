e5f3a62b1d3deb2fbf528a9791f51f924702d4b5
Nikodem Okroj <n.okroj@icloud.com>
2025-04-04 09:23:31 +0200

    feat(frontend): Update GameplayPage to display current sequence part and player statistics, enhancing user interface with detailed question and player information

--- Frontend/app/game/gameplay/page.tsx
+++ Frontend/app/game/gameplay/page.tsx
@@ -4,11 +4,23 @@ import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import LoadingDialog from "@/app/components/loadingDialog";
 import { getAllData, validateGameToken } from "@/app/actions/gameplay";
-import { addToast, Button, Card, CardBody, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/react";
+import {
+  addToast,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@heroui/react";
 import { GamePlayer } from "@/app/models/GamePlayer";
 import { SequenceQuestion } from "@/app/models/SequenceQuestion";
 import { GameAction } from "@/app/models/GameAction";
 import { BanIcon, EllipsisVerticalIcon, ShieldX, StopCircleIcon } from "lucide-react";
+import Step1Page from "./step1Page";
 
 export default function GameplayPage() {
   const [gameToken, setGameToken] = useState<string | null>(null);
@@ -20,6 +32,7 @@ export default function GameplayPage() {
   const [currentPlayer, setCurrentPlayer] = useState<GamePlayer | null>(null);
   const [currentQuestion, setCurrentQuestion] =
     useState<SequenceQuestion | null>(null);
+  const [currentSequencePart, setCurrentSequencePart] = useState<number>(0);
   const [actions, setActions] = useState<GameAction[]>([]);
   const [startedAt, setStartedAt] = useState<Date | null>(null);
   const [updatedAt, setUpdatedAt] = useState<Date | null>(null);
@@ -57,6 +70,7 @@ export default function GameplayPage() {
     setActions(res.data.actions);
     setStartedAt(new Date(res.data.startTime));
     setUpdatedAt(new Date(res.data.updatedAt));
+    setCurrentSequencePart(res.data.currentSequencePart);
   }, [gameToken, router]);
 
   const handleValidateGameToken = useCallback(async () => {
@@ -131,16 +145,63 @@ export default function GameplayPage() {
             </div>
           </CardBody>
         </Card>
-        <div className="max-w-4xl w-full h-full flex flex-col gap-4">
-          <Card>
-            <CardBody>
-              <div className="flex flex-row gap-2">
-                <div className="text-2xl font-bold max-w-[70%] truncate">
-                  {gameName}
-                </div>
-              </div>
-            </CardBody>
-          </Card>
+        <div className="max-w-4xl w-full h-full flex flex-col gap-4 text-center">
+          <div className="flex flex-col gap-4 w-full items-center">
+            <div>
+              Etap{" "}
+              {currentSequencePart === 0
+                ? "Pierwszy"
+                : currentSequencePart === 1
+                  ? "Drugi"
+                  : "Trzeci"}
+            </div>
+            <div className="flex flex-row gap-4">
+              <Card className="text-left flex flex-col gap-2 items-start justify-start w-fit">
+                <CardHeader className="text-2xl font-bold">
+                  Pytanie: {currentQuestion?.question.content}
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <div className="text-xl font-bold text-gray-300">
+                    Wariant 2: {currentQuestion?.question.variant2}
+                  </div>
+                  <div className="text-xl font-bold text-gray-300">
+                    Wariant 3: {currentQuestion?.question.variant3 ?? "Brak"}
+                  </div>
+                  <div className="text-xl font-bold text-gray-500">
+                    Odpowiedź: {currentQuestion?.question.correctAnswer}
+                  </div>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardHeader className="text-2xl font-bold">
+                  Aktualny gracz: {currentPlayer?.name}
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <div className="text-2xl font-bold text-white text-center">
+                    {currentPlayer?.name}
+                  </div>
+                  <div className="text-2xl font-bold text-white text-center">
+                    Punkty:{" "}
+                    <span className="text-primary">
+                      {currentPlayer?.points}
+                    </span>
+                  </div>
+                  <div className="text-2xl font-bold text-white text-center">
+                    Szanse:{" "}
+                    <span className="text-primary">{currentPlayer?.lives}</span>
+                    /3
+                  </div>
+                </CardBody>
+              </Card>
+            </div>
+            <Card>
+              <CardBody>
+                
+              </CardBody>
+            </Card>
+          </div>
         </div>
       </div>
       <LoadingDialog isLoading={isLoading} blackBackground={true} />
